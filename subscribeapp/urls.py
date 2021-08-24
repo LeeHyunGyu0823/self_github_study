ad154d7ea43d66a36e5ffc782971583c058963d0
LeeHyunGyu0823 <karisiz700@gmail.com>
2021-08-24 14:52:37 +0900

    self study 2021 08 24

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -5,5 +5,6 @@ from subscribeapp.views import SubscriptionView
 app_name = 'subscribeapp'
 
 urlpatterns = [
-    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe')
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
